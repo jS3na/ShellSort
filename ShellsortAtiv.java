4c966f0d8e979b0669365a94985ad8e9d0737a08
João Sena <126720426+jS3na@users.noreply.github.com>
2023-11-28 22:54:58 -0300

    VetorUpdatev2

--- ShellsortAtiv.java
+++ ShellsortAtiv.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
+import java.util.Random;
+
 class ShellsortAtiv {
 
-    public static int tamVet = 4;
+    public static int tamVet;
     public static int h = 1;
-    public static int temp;
+    public static int temp, op;
 
-    public static void main(String[] args) {
+    public static void ShellsortVetor(){
 
-        int listaNum[] = {3, 9, 10, 1};
+        Random escolha = new Random();
+
+        int listaNum[] = new int[tamVet];
+
+        for (int i = 0; i < tamVet; i++) {
+            listaNum[i] = escolha.nextInt(100) + 1;
+        }
 
         while (h <= tamVet / 3) {
             h = h * 3 + 1;
@@ -17,21 +26,44 @@ class ShellsortAtiv {
             for (int i = h; i < tamVet; i++) {
 
                 temp = listaNum[i];
-                int j = i;
+                int temp2 = i;
 
-                while (j >= h && listaNum[j - h] > temp) {
-                    listaNum[j] = listaNum[j - h];
-                    j -= h;
+                while (temp2 >= h && listaNum[temp2 - h] > temp) {
+                    listaNum[temp2] = listaNum[temp2 - h];
+                    temp2 -= h;
                 }
 
-                listaNum[j] = temp;
+                listaNum[temp2] = temp;
             }
 
             h /= 3;
         }
 
+        System.out.println("\n");
+
         for (int i : listaNum) {
             System.out.println(i);
         }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Escolha o que deseja fazer com o método de ordenação ShellSort:\n1 - Ordenação de um VETOR aleatório");
+        op = scan.nextInt();
+
+        switch(op){
+
+            case 1:
+
+                while(true){
+                    System.out.println("Selecione o tamanho do vetor:\n4\n6\n10\n12");
+                    tamVet = scan.nextInt();
+
+                    if(tamVet != 4 && tamVet != 6 && tamVet != 10 && tamVet != 12) System.out.println("Opção inválida!Digite novamente!\n");
+                    else {ShellsortVetor(); break;}
+                }
+    }
     }
 }
